9e754a1d6e7ee63864fad04d6d42d024ceb7474e
IcePlayer <nathanchevalet@orange.fr>
2022-06-17 14:39:57 +0200

    remove header

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,20 +1,12 @@
 <template>
-  <section>
-      <Header/>
-  </section>
   <section></section>
   <section></section>
   <section></section>
 </template>
 
 <script>
-import Header from '@/components/Header.vue'
-
 export default {
   name: 'Home',
-  components: {
-    Header
-  }
 }
 </script>
 
