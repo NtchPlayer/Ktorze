19540b375b3c8bb6a40f5a5478cae2f06f9dd714
RenaudVernageau <renaudvrng@gmail.com>
2022-06-17 16:37:52 +0200

    add section bottom

---
+++ src/components/SectionBottom.vue
@@ -0,0 +1,38 @@
+<template>
+  <div class="sectionBottom">
+    <figure>  
+      <img src="@/assets/jacob.png" alt="">
+    </figure>
+
+    <h2>Section Bottom</h2>
+    <router-link :to="{name: 'home'}">CTA</router-link>
+  </div>
+</template>
+
+<script>
+export default {
+    name: 'SectionBottom'
+}
+</script>
+
+<style lang="scss">
+
+.sectionBottom{
+    height: 400px;
+    position:relative;
+    overflow: hidden;
+    display: flex;
+    flex-direction:column;
+    justify-content: center;
+    img{
+        position:absolute;
+        min-height: 600px;
+        width: 100%;
+        top: 50%;
+        left: 50%;
+        transform: translate(-50%, -50%);
+        z-index:-1;
+    }
+}
+
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,17 +2,20 @@
     <Fotorama/>
     <SectionHome/>
     <SectionHome/>
+    <SectionBottom/>
 </template>
 
 <script>
 import Fotorama from '@/components/Fotorama.vue'
 import SectionHome from '@/components/SectionHome.vue'
+import SectionBottom from '@/components/SectionBottom.vue'
 
 export default {
   name: 'Home',
   components: {
     Fotorama,
-    SectionHome
+    SectionHome,
+    SectionBottom
   }
 }
 </script>
