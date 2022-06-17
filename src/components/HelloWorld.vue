95b7123d88434d417aac2247c93e5d97299ea278
IcePlayer <nathanchevalet@orange.fr>
2022-06-17 13:34:11 +0200

    update style tag

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -39,20 +39,9 @@ export default {
 }
 </script>
 
-<!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
-h3 {
-  margin: 40px 0 0;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
+<style lang="scss">
+h1{
+ font-size: 1.5rem;
+ color: red;
 }
 </style>
