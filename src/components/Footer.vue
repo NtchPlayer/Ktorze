de8e02ec4406f8fd099305a24a62cabd46c175ef
OUAIS MON FRERE <jeralexis77@aol.com>
2022-06-17 23:29:06 +0200

    footer

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,7 +1,45 @@
 <template>
     <footer>
+        <div class="footerMain">
+            <figure class="ftLogo">
+                <p>Site crée par l'agence</p>
+                <img src="@/assets/footerLogo.png" alt="">
+            </figure>
+            
+            <figure class="ftOutdoor">
 
-        <h1>Foot Foot Footer</h1>
+                <h4 class="ftTitle">Outdoor</h4>
+
+                <p>L'équipe</p>
+                <p> Notre projet</p>
+                <p>Les fonctionnalités</p>
+
+            </figure>
+            
+            <figure class="ftAdress">
+
+                <h4 class="ftTitle">Notre adresse</h4>
+
+                <p>3 Rte d'Opme</p>
+                <p>63450 Chanonat</p>
+                <p>France</p>
+
+            </figure>
+            
+            <figure class="ftSocialM">
+                <img src="@/assets/fb.png" alt="">
+                <img src="@/assets/insta.png" alt="">
+                <img src="@/assets/you.png" alt="">
+
+                <h4></h4>
+
+            </figure>
+
+        </div>
+        
+        <div class="footerCopyright">
+            <h4>©2022 GrounDesk. All right reserved</h4>
+        </div>
 
     </footer>
   
@@ -12,6 +50,8 @@ export default {
     name: 'Footer'
 }
 
+
+
 </script>
 
     
@@ -19,14 +59,48 @@ export default {
 
 
 footer{
-    height: 400px;
+    height: 382px;
     display: flex;
     justify-content: center;
     align-items: center;
-    background-color: green;
+    background-color: #F9F9F9;
 
-    h1{
-        font-size: 3rem;
+    .footerMain{
+        height: 314px;
+        display: flex;
+        justify-content: space-around;
+        align-items: center;
+        background-color: #F9F9F9;
+
+        .ftTitle{
+            font-weight: bold;
+        }
+
+        .ftLogo{
+            width: 190px;
+            height: 170px;
+            display: flex;
+            flex-direction: column;
+            align-items: center;
+            justify-content: space-between ;
+        }
+
+        .ftOutdoor, .ftAdress{
+            width: 150px;
+            height: 168px;
+            display: flex;
+            justify-content: space-between;
+            align-items: flex-start;
+            flex-direction: column;
+        }
+
+    }
+
+    .footerCopyright{
+        height: 68px;
+        display: flex;
+        justify-content: center;
+        align-items: center;
     }
 }
 
