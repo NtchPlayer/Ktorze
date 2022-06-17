f6c49fd44cce1ce0889d00c823802c1e5a1099b6
OUAIS MON FRERE <jeralexis77@aol.com>
2022-06-17 21:38:18 +0200

    1st iso fotorama + nav

--- src/components/Fotorama.vue
+++ src/components/Fotorama.vue
@@ -1,10 +1,12 @@
 <template>
   <section class="fotorama">
+    <router-link class="btn-primary btn-primary-fotorama" :to="{name: 'home'}">Télécharger l'application</router-link>
     <figure>
-      <img alt="Vue logo" src="@/assets/logo.png">
+      <img alt="Vue logo" src="@/assets/coverfotorama.png">
     </figure>
-    <h1>Lorem ipsum</h1>
-    <router-link :to="{name: 'home'}">CTA</router-link>
+    <h1>En avant la marche avec Outdoor</h1>
+    <p>La première application mobile pour les randonneurs<br>qui saura combler votre coeur d’aventurier en toute sérénité !</p>
+    <router-link class="btn-primary btn-primary-fotorama-center" :to="{name: 'home'}">Découvrir l'application</router-link>
   </section>
 </template>
 
@@ -16,20 +18,49 @@ export default {
 
 <style lang="scss">
  .fotorama{
-  min-height: 600px;
+  height: 860px;
   position: relative;
   overflow: hidden;
   display: flex;
   flex-direction: column;
   justify-content: center;
-  img{
+  z-index: 0;
+  h1{
+    font-size: 2.2rem;
+    font-weight: 600;
+    color:#00704e;
     position: absolute;
-    min-height: 600px;
-    width: 100%;
-    top: 50%;
-    left: 50%;
-    transform: translate(-50%, -50%);
-    z-index: -1;
+    top: 35%;
+    left: 30%;
+    &~p{
+      text-align: left;
+      position: absolute;
+      top: 42%;
+      left: 30%;
+      color:#00704e;
+    }
+  }
+  figure{
+      position: absolute;
+      height: 860px;
+      width: 100%;
+      top: 0;
+      z-index: -1;
+      img{
+        width: 100%;
+      }
+  }
+  .btn-primary-fotorama{
+    position: absolute;
+    top: 20px;
+    right: 20px;
+    &-center{
+      position: absolute;
+      top: 60%;
+      left: 50%;
+      transform: translate(-50%, -50%);
+    }
+
   }
  }
 </style>
