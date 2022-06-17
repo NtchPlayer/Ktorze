b2dd5c8f9689643d414a872374eb69a4119a4e55
IcePlayer <nathanchevalet@orange.fr>
2022-06-17 14:39:49 +0200

    updade for vue router

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,16 @@
 <template>
-  <Home/>
+  <Header/>
+  <router-view />
 </template>
 
 <script>
-import Home from './views/Home.vue'
-
+import Header from '@/components/Header.vue'
 
 export default {
   name: 'App',
   components: {
-    Home
+    Header
   }
-
 }
 </script>
 
