a89d25e53a72239c6730a9408aa79815951a17de
IcePlayer <nathanchevalet@orange.fr>
2022-06-18 13:18:08 +0200

    add gtmInstance to app

--- src/main.js
+++ src/main.js
@@ -1,9 +1,11 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
+import { gtmInstance } from './gtm-config'
 
 const app = createApp(App)
 
 app.use(router)
+app.use(gtmInstance)
 
 router.isReady().then(() => app.mount('#app'))
