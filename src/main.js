803c33c306b5bfe0b1b38b0d39f85dbf5238bdac
IcePlayer <nathanchevalet@orange.fr>
2022-06-25 11:18:11 +0200

    add vue-meta

--- src/main.js
+++ src/main.js
@@ -2,15 +2,16 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 import { gtmInstance } from './gtm-config'
+import { metaManager, vueMetaPlugin } from './vue-meta-config'
 
-import * as Vue from 'vue'
 import axios from 'axios'
 import VueAxios from 'vue-axios'
-
 
 const app = createApp(App)
 
 app.use(router)
+app.use(metaManager)
+app.use(vueMetaPlugin)
 app.use(gtmInstance)
 app.use(VueAxios, axios)
 
