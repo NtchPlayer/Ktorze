2390019c5b31126919c116fab72ab455921c209e
IcePlayer <nathanchevalet@orange.fr>
2022-06-26 20:13:44 +0200

    add ApiService

--- src/main.js
+++ src/main.js
@@ -1,18 +1,19 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
+import ApiService from './common/api.service'
 import { gtmInstance } from './gtm-config'
 import { metaManager, vueMetaPlugin } from './vue-meta-config'
 
-import axios from 'axios'
-import VueAxios from 'vue-axios'
-
-const app = createApp(App)
+export const app = createApp(App)
 
 app.use(router)
 app.use(metaManager)
 app.use(vueMetaPlugin)
 app.use(gtmInstance)
-app.use(VueAxios, axios)
 
-router.isReady().then(() => app.mount('#app'))
+router.isReady().then(() => {
+  app.mount('#app')
+})
+
+ApiService.init()
