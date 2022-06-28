3e7a09f77d076089babce0bc4c1865bad8ff18bf
IcePlayer <nathanchevalet@orange.fr>
2022-06-29 01:04:56 +0200

    update meta tags

--- src/App.vue
+++ src/App.vue
@@ -46,8 +46,79 @@ export default {
         }, {
           name: 'apple-mobile-web-app-title',
           content: 'Balise360'
-        }
+        }, {
+          name: 'og:title',
+          content: 'Balise 360, la solution de tous les randonneurs engagés'
+        }, {
+          name: 'og:description',
+          content: 'Balise 360, la solution qui permet à tous les randonneurs passionnés et engagés de préparer ses itinéraires de randonnées, de se repérer avec ses cartes numériques et son GPS intégré.'
+        }, {
+          name: 'og:url',
+          content: process.env.VUE_APP_BASE_SITE
+        }, {
+          name: 'og:type',
+          content: 'website'
+        }, {
+          name: 'og:site_name',
+          content: 'Balise360'
+        }, {
+          name: 'og:local',
+          content: 'fr_FR'
+        }, {
+          name: 'og:image',
+          content: `http://www.balise360.fr/open_graph.jpg`
+        }, {
+          name: 'og:image:secure_url',
+          content: `${process.env.VUE_APP_BASE_SITE}/open_graph.jpg`
+        }, {
+          name: 'og:image:type',
+          content: 'image/jpg'
+        }, {
+          name: 'og:image:width',
+          content: '1200'
+        }, {
+          name: 'og:image:height',
+          content: '630'
+        }, {
+          name: 'og:image:alt',
+          content: 'Logo Balise 360'
+        }, {
+          name: 'twitter:title',
+          content: 'Balise 360, la solution de tous les randonneurs engagés'
+        }, {
+          name: 'twitter:description',
+          content: 'Balise 360, la solution qui permet à tous les randonneurs passionnés et engagés de préparer ses itinéraires de randonnées, de se repérer avec ses cartes numériques et son GPS intégré.'
+        }, {
+          name: 'twitter:url',
+          content: process.env.VUE_APP_BASE_SITE
+        }, {
+          name: 'twitter:site',
+          content: '@balise360.fr'
+        }, {
+          name: 'twitter:creator',
+          content: '@Balise360 DevTeam'
+        }, {
+          name: 'twitter:image',
+          content: `${process.env.VUE_APP_BASE_SITE}/twitter_card.png`
+        }, {
+          name: 'twitter:card',
+          content: 'summary_large_image'
+        }, {
+          name: 'twitter:title',
+          content: ''
+        }, {
+          name: 'twitter:title',
+          content: ''
+        },
       ],
+      link: [
+        { rel: 'favicon', href: './favicon.ico' },
+        { rel: 'icon', type: 'image/png', href: './icons/icon.png' },
+        { rel: 'apple-touch-icon', type: 'image/png', sizes: '152x152', href: './icons/touch-icon-ipad.png' },
+        { rel: 'apple-touch-icon', type: 'image/png', sizes: '180x180', href: './icons/touch-icon-iphone-retina.png' },
+        { rel: 'apple-touch-icon', type: 'image/png', sizes: '167x167', href: './icons/touch-icon-ipad-retina.png' },
+        { rel: 'apple-touch-icon', type: 'image/png', sizes: '512x512', href: './icons/apple-touch-icon.png' }
+      ]
     })
 
     return {
