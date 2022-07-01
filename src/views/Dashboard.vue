f006b848c97fac6d6c5e1635dbe982c7e6d0f8bb
RenaudVernageau <renaudvrng@gmail.com>
2022-07-01 12:34:06 +0200

    add dashboard 2

--- src/views/Dashboard.vue
+++ src/views/Dashboard.vue
@@ -2,14 +2,27 @@
   <div class="dashboard">
     <h1>Dashboard</h1>
     <p>{{info}}</p>
+    <vue-instagram token="IGQVJXalBCcnlVNC1Dc1hCYm1fMEVzTkVMcDNPTlRNTk1UdWo1OGIwSFVXcTdSdmRwZAWdaSkF3MFBKZAUFaTGVFeGhXWVhCUHlqX2hfQXVYOEh5ZA0JvRmZAZAcWhEMGhOLTRyZAmc1Q1NEYUVzNE9PaGU4QwZDZD" username="balise360" :count="3">
+        <template slot="feeds" scope="props">
+            <li class="instagram-media fancy-list"> {{ props.feed.link }} </li>
+            <img :src=" props.feed.images.standard_resolution.url " alt="">
+        </template>
+        <template slot="error" scope="props">
+            <div class="fancy-alert"> {{ props.error.error_message }} </div>
+        </template>
+    </vue-instagram>
   </div>
 </template>
 
 <script>
 import axios from 'axios'
+import VueInstagram from 'vue-instagram'
 
 export default {
     name: 'Dashboard',
+    components: {
+        VueInstagram
+    },
     data () {
         return{
             info: null
